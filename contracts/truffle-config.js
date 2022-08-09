85c0b66cde0c1f91411b36ccd8f6025394b7406e
Matt Lefebvre <matthewelefebvre@gmail.com>
2022-08-08 22:46:19 -0400

    goerli migration

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -18,10 +18,9 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = JSON.parse(fs.readFileSync("./secret.json").toString()).mnemonic;
 
 module.exports = {
   /**
@@ -59,14 +58,12 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // ropsten: {
-    //   provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    //   network_id: 3,       // Ropsten's id
-    //   gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/a829a673a5a74985a0d99a401982c941`),
+      network_id: '5',
+      gas: 5500000,
+      gasPrice: 10000000000,
+    },
     //
     // Useful for private networks
     // private: {
