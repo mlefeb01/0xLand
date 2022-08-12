7ccf5a59b3c2cfcc43bcda1485f6e163d4272892
Matt Lefebvre <matthewelefebvre@gmail.com>
2022-08-12 16:19:17 -0400

    Dynamically create keypair which READ ONLY calls are made from

--- plugin/src/main/java/io/zeroexland/service/contract/ContractService.java
+++ plugin/src/main/java/io/zeroexland/service/contract/ContractService.java
@@ -3,11 +3,13 @@ package io.zeroexland.service.contract;
 import io.zeroexland.config.ConfigYml;
 import io.zeroexland.contract.LandContract;
 import org.web3j.crypto.Credentials;
+import org.web3j.crypto.ECKeyPair;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.gas.StaticGasProvider;
 
 import java.math.BigInteger;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ContractService implements IContractService {
     private LandContract landContract;
@@ -16,7 +18,7 @@ public class ContractService implements IContractService {
     public void load(ConfigYml configYml) {
         try {
             final Web3j web3j = Web3j.build(new HttpService(configYml.getNetwork()));
-            final Credentials credentials = Credentials.create(configYml.getPrivateKey());
+            final Credentials credentials = Credentials.create(ECKeyPair.create(BigInteger.valueOf(ThreadLocalRandom.current().nextLong())));
             this.landContract = LandContract.load(
                     configYml.getAddress(),
                     web3j,
